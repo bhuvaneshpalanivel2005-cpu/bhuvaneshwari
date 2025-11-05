114b78174f7cdaebb83820978a5733ec84c19608
bhuvaneshpalanivel2005-cpu <bhuvanesh.palanivel2005@gmail.com>
2025-11-05 12:53:08 +0530

    Add files via upload

---
+++ mini calculater.py
@@ -0,0 +1,14 @@
+print("mini calculater")
+a=int(input("a:"))
+b=int(input("b:"))
+operation=input("add/sub/mul/div:")
+if(operation=="add"):
+      prit(a+b)
+elif(operation=="sub"):
+    print(a-b)
+elif(operation=="mul"):
+    print(a*b)
+elif(operation=="div"):
+    print(a/b)
+else:
+    print("invalid operation")
