9584ea17082f72f1c8d72ab4378c1d5ecc39f185
bhuvaneshpalanivel2005-cpu <bhuvanesh.palanivel2005@gmail.com>
2025-11-05 11:19:43 +0530

    Add files via upload

---
+++ bhuvana/name.py
@@ -0,0 +1,18 @@
+print("what is your first name?")
+first_name=input()
+print("what is your last name?")
+last_name=input()
+print("Hey"+first_name+""+last_name+"!")
+dictionary_1={
+    "computer program": "A series of instructions that can be executed by computer", "syntax": "the rules we create for computers and programmers to follow to avoid ambiguity a ""programming Language" : "A formal set of syntax for writing a computer programe"
+    }
+dictionary_2={
+    "pilot";"James",
+    "co-pilot";"paul",
+    "steward":[
+        "peter",
+        "card",
+        "Jane"
+        ]
+    "breakfast":list_2
+    "passengers":2025
---
+++ bhuvana/sample1.py
@@ -0,0 +1,12 @@
+print("hello,world!");
+
+
+
+
+
+name=input()
+score=int(input())
+department=input()
+print("my name is;",name)
+print("my score is;",score/10,"/10")
+print("my department is;",department)
