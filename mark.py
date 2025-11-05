fe18093295d451c9efad126194fc31c468fdb9cc
bhuvaneshpalanivel2005-cpu <bhuvanesh.palanivel2005@gmail.com>
2025-11-05 12:03:41 +0530

    Add files via upload

---
+++ mark.py
@@ -0,0 +1,8 @@
+print("mark>35 print pass,else print false")
+name=input("enter your name:")
+Register_Number=int(input("enter your Register_Number:"))
+mark=int(input("enter your mark:"))
+if(mark>35):
+    print("pass")
+else:
+    print("fail")
